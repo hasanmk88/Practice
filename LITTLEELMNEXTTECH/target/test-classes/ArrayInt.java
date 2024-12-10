df40faa8173392093ef06d3976e9fe19bd02ab01
hasanmk88 <hasanmk88@gmail.com>
2024-12-10 14:44:06 -0600

    Code transfer

---
+++ LITTLEELMNEXTTECH/target/test-classes/ArrayInt.java
@@ -0,0 +1,14 @@
+
+public class ArrayInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {10,11,3,24};
+		for (i=0;i<arr.length;i++) {
+			System.out.println(arr);
+			
+		}
+
+	}
+
+}
